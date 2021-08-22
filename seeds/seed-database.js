372f805d6cbb1bb1c63a82a9951c65d008b8f42e
giannifontanot <giannifontanot@gmail.com>
2021-08-22 11:41:27 -0500

    Seeder not working yet

---
+++ seeds/seed-database.js
@@ -0,0 +1,19 @@
+const sequelize = require('../config/connection');
+const {User, Message, Comment} = require('../models/index');
+
+const user_seeds = require('./user.json');
+const message_seeds = require('./message.json');
+const comment_seeds = require('./comment.json');
+
+
+const sow = async () => {
+    const sequelizeResult = await sequelize.sync({force:true});
+    const seedUserResult = await User.bulkCreate(user_seeds);
+    const seedMessageResult = await Message.bulkCreate(message_seeds);
+    const seedCommentResult = await Comment.bulkCreate(comment_seeds);
+    process.exit(0);
+}
+
+
+// Seed the database
+sow();
