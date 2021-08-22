2a88bb6bcd0882fa3bf031a9f039118e957291d9
giannifontanot <giannifontanot@gmail.com>
2021-08-22 12:19:22 -0500

    Seeder not working yet

--- models/index.js
+++ models/index.js
@@ -2,10 +2,17 @@ const User = require('./User');
 const Message = require('./Message');
 const Comment = require('./Comment');
 
-User.hasMany(Message,{foreignKey:'username', onDelete:'CASCADE'});
+
 Message.belongsTo(User, {foreignKey:'username', onDelete:'CASCADE'});
 
 Message.hasMany(Comment, {foreignKey:'message_id', onDelete:'CASCADE'});
-Comment.belongsTo(Message, {foreignKey:'message_id', onDelete:'CASCADE'});
+Comment.belongsTo(User, {foreignKey:'user_id', onDelete:'CASCADE'});
+
+
+// User.hasMany(Message,{foreignKey:'username', onDelete:'CASCADE'});
+// Message.belongsTo(User, {foreignKey:'username', onDelete:'CASCADE'});
+//
+// Message.hasMany(Comment, {foreignKey:'message_id', onDelete:'CASCADE'});
+// Comment.belongsTo(Message, {foreignKey:'message_id', onDelete:'CASCADE'});
 
 module.exports = {User, Message, Comment}
