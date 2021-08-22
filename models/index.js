cf8c1bba7e659f7fd7a360d1b4045040f615e2b3
giannifontanot <giannifontanot@gmail.com>
2021-08-22 13:03:46 -0500

    Seeder not working yet

--- models/index.js
+++ models/index.js
@@ -2,27 +2,27 @@ const User = require('./User');
 const Message = require('./Message');
 const Comment = require('./Comment');
 
-
-Message.belongsTo(User, {foreignKey:'user_id', onDelete:'CASCADE'});
-
-Message.hasMany(Comment, {foreignKey:'message_id', onDelete:'CASCADE'});
-Comment.belongsTo(User, {foreignKey:'user_id', onDelete:'CASCADE'});
-
-
-
-
-
-
-
-
-
-// User.hasMany(Message,{foreignKey:'user_id', onDelete:'CASCADE'});
+//
 // Message.belongsTo(User, {foreignKey:'user_id', onDelete:'CASCADE'});
 //
 // Message.hasMany(Comment, {foreignKey:'message_id', onDelete:'CASCADE'});
-// Comment.belongsTo(Message, {foreignKey:'message_id', onDelete:'CASCADE'});
-//
-// User.hasMany(Comment,{foreignKey:'user_id', onDelete:'CASCADE'});
-// Comment.belongsTo(User,{foreignKey:'user_id', onDelete:'CASCADE'});
+// Comment.belongsTo(User, {foreignKey:'user_id', onDelete:'CASCADE'});
+
+
+
+
+
+
+
+
+
+User.hasMany(Message,{foreignKey:'user_id', });
+Message.belongsTo(User, {foreignKey:'user_id', });
+
+Message.hasMany(Comment, {foreignKey:'message_id', });
+Comment.belongsTo(Message, {foreignKey:'message_id',});
+
+User.hasMany(Comment,{foreignKey:'user_id',});
+Comment.belongsTo(User,{foreignKey:'user_id', });
 
 module.exports = {User, Message, Comment}
